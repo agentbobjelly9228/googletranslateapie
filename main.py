66f56e82e9a892afa678c9f71b1d5c47c44c1c1c
Alex Man <agentbobjelly@gmail.com>
2021-05-27 12:38:19 -0700

    hi

--- main.py
+++ main.py
@@ -16,31 +16,31 @@ def index():
 
 @app.route('/translate', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        words = request.args['words']
-        lang = request.args['lang']
-        print(translator.translate(words, dest=lang).text)
-        return jsonify({'response': translator.translate(words, dest=lang).text})
-    else:
-        return 'hello world'
+  if request.method == 'POST':
+    words = request.args['words']
+    lang = request.args['lang']
+    print(translator.translate(words, dest=lang).text)
+    return jsonify({'response': translator.translate(words, dest=lang).text})
+  else:
+    return 'hello world'
 
 
 @app.route('/langs', methods=['POST', 'GET'])
 def langs():
-    if request.method == 'POST':
-        langg = LANGUAGES
-        langs = []
-        codes = []
-        for i in langg:
-            codes.append(i)
-            langs.append(langg[i])
-        print(langs)
-        print(codes)
-        stuff = {'languages': langs, 'codes': codes}
-        return jsonify(stuff)
-    else:
-        return 'hi there'
+  if request.method == 'POST':
+    langg = LANGUAGES
+    langs = []
+    codes = []
+    for i in langg:
+      codes.append(i)
+      langs.append(langg[i])
+    print(langs)
+    print(codes)
+    stuff = {'languages': langs, 'codes': codes}
+    return jsonify(stuff)
+  else:
+    return 'hi there'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
