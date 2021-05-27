a915bab3b79efd1da299f9f60c096d121a64f486
Alex Man <agentbobjelly@gmail.com>
2021-05-27 12:41:16 -0700

    hi

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from googletrans import Translator, LANGUAGES
+
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+thing = ''
+try: 
+  from flask_cors import CORS
+  from googletrans import Translator, LANGUAGES
+except Exception as e:
+  things = e
 
 app = Flask(__name__)
 CORS(app)
@@ -11,35 +16,35 @@ translator = Translator()
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return things
 
 
-@app.route('/translate', methods=['POST', 'GET'])
-def home():
-  if request.method == 'POST':
-    words = request.args['words']
-    lang = request.args['lang']
-    print(translator.translate(words, dest=lang).text)
-    return jsonify({'response': translator.translate(words, dest=lang).text})
-  else:
-    return 'hello world'
+# @app.route('/translate', methods=['POST', 'GET'])
+# def home():
+#   if request.method == 'POST':
+#     words = request.args['words']
+#     lang = request.args['lang']
+#     print(translator.translate(words, dest=lang).text)
+#     return jsonify({'response': translator.translate(words, dest=lang).text})
+#   else:
+#     return 'hello world'
 
 
-@app.route('/langs', methods=['POST', 'GET'])
-def langs():
-  if request.method == 'POST':
-    langg = LANGUAGES
-    langs = []
-    codes = []
-    for i in langg:
-      codes.append(i)
-      langs.append(langg[i])
-    print(langs)
-    print(codes)
-    stuff = {'languages': langs, 'codes': codes}
-    return jsonify(stuff)
-  else:
-    return 'hi there'
+# @app.route('/langs', methods=['POST', 'GET'])
+# def langs():
+#   if request.method == 'POST':
+#     langg = LANGUAGES
+#     langs = []
+#     codes = []
+#     for i in langg:
+#       codes.append(i)
+#       langs.append(langg[i])
+#     print(langs)
+#     print(codes)
+#     stuff = {'languages': langs, 'codes': codes}
+#     return jsonify(stuff)
+#   else:
+#     return 'hi there'
 
 
 if __name__ == '__main__':
