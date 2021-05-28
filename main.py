08fb41990ead8bb9639714e9ceda5062283bd069
Alex Man <agentbobjelly@gmail.com>
2021-05-27 18:50:24 -0700

    hiiiiii

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-from flask import Flask, redirect, url_for, render_template
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from googletrans import Translator, LANGUAGES
 
 app = Flask(__name__)
+CORS(app)
+
+
+translator = Translator()
 
 
 @app.route('/')
+def index():
+    return 'things'
+
+
+@app.route('/translate', methods=['POST', 'GET'])
 def home():
-    return 'hi'
+  if request.method == 'POST':
+    words = request.args['words']
+    lang = request.args['lang']
+    print(translator.translate(words, dest=lang).text)
+    return jsonify({'response': translator.translate(words, dest=lang).text})
+  else:
+    return 'hello world'
+
+
+@app.route('/langs', methods=['POST', 'GET'])
+def langs():
+  if request.method == 'POST':
+    langg = LANGUAGES
+    langs = []
+    codes = []
+    for i in langg:
+      codes.append(i)
+      langs.append(langg[i])
+    print(langs)
+    print(codes)
+    stuff = {'languages': langs, 'codes': codes}
+    return jsonify(stuff)
+  else:
+    return 'hi there'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
