913c38534744d661a95fa2d7453b890945139f52
Alex Man <agentbobjelly@gmail.com>
2021-05-27 18:40:07 -0700

    hi

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
-
-from flask import Flask, jsonify, request
-# try: 
-# from flask_cors import CORS
-#   from googletrans import Translator, LANGUAGES
-# except Exception as e:
-#   things = e
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-# CORS(app)
-
-
-# translator = Translator()
 
 
 @app.route('/')
-def index():
-  return 'things'
-
-
-# @app.route('/translate', methods=['POST', 'GET'])
-# def home():
-#   if request.method == 'POST':
-#     words = request.args['words']
-#     lang = request.args['lang']
-#     print(translator.translate(words, dest=lang).text)
-#     return jsonify({'response': translator.translate(words, dest=lang).text})
-#   else:
-#     return 'hello world'
-
-
-# @app.route('/langs', methods=['POST', 'GET'])
-# def langs():
-#   if request.method == 'POST':
-#     langg = LANGUAGES
-#     langs = []
-#     codes = []
-#     for i in langg:
-#       codes.append(i)
-#       langs.append(langg[i])
-#     print(langs)
-#     print(codes)
-#     stuff = {'languages': langs, 'codes': codes}
-#     return jsonify(stuff)
-#   else:
-#     return 'hi there'
+def home():
+    return 'hi'
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
